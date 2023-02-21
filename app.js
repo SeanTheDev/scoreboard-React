2a72db18a842bacd4758dae147e697611545952c
Sean-Patrick <rowan.seanp@gmail.com>
2023-02-21 09:15:22 -0500

    minor edits

--- app.js
+++ app.js
@@ -1,32 +1,32 @@
-function Header() {
+function Header(props) {
   return (
     <header>
-      <h1>Scoreboard</h1>
-      <span className="stats">Players: 1</span>
+      <h1>{ props.title }</h1>
+      <span className="stats">Players: { props.totalPlayers }</span>
     </header>
   );
 }
 
 // create a new component for a player that returns a div with a class of player and a nested div with a class of player-name
-function Player() {
+function Player(props) {
 
   return (
     <div className="player">
       <div className="player-name">
-        Sean
+        {props.name}
       </div>
-      <Counter />
+      <Counter score={props.score}/>
     </div>
   );
 
 }
 
-function Counter(){
+function Counter(props){
 
   return (
     <div className="counter">
       <button className="counter-action decrement"> - </button>
-      <span className="counter-score">35</span>
+      <span className="counter-score">{props.score}</span>
       <button className="counter-action increment"> + </button>
     </div>
   );
@@ -37,9 +37,12 @@ function App(){
 
   return (
     <div className="scoreboard">
-      <Header />
+      <Header title="Scoreboard" totalPlayers={1}/>
       {/* Players list */}
-      <Player />
+      <Player name={"Sean"} score={100} />
+      <Player name={"John"} score={99} />
+      <Player name={"James"} score={20} />
+      <Player name={"Susan"} score={50} />
     </div>
   );
 
